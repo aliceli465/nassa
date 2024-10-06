fb5c70a2a7c0cc67ac703423672195181e81572c
wiidu <wiiidu315@gmail.com>
2024-10-05 22:00:39 -0500

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import BallScene from "./components/ball";
 import Sidebar from "./components/sidebar";
 import "./App.css";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar'; // You can omit the ".jsx" in most cases.
+
 function App() {
   const [sunSize, setSunSize] = useState(1); // sunSize of the larger ball
   const [planetSize, setPlanetSize] = useState(0.2); // sunSize of the smaller ball
@@ -30,8 +34,26 @@ function App() {
     setHeat(event.target.value);
   };
 
+  const Home = () => <h1>Home Page</h1>;
+  const About = () => <h1>About Page</h1>;
+  const Sources = () => <h1>Sources & References</h1>;
+  const NotFoundPage = () => <h1>404 - Page Not Found</h1>;
+
   return (
     <>
+    <div>
+      <Router>
+      <Navbar />
+      <div style={{ padding: '2rem' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sources" element={<Sources />} />
+          <Route path="*" element={<NotFoundPage />} />  {/* Catch-all route */}
+        </Routes>
+      </div>
+    </Router>
+    </div>
       <div style={{ textAlign: "center" }}>
         <h1>No Diddy</h1>
       </div>
